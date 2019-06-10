4170a7ccdaf206044dbc5a90594c2192afb5ed9b
Akshay <akshaycm@hotmail.com>
2019-06-10 16:28:37 +0300

    hello world react material

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
-import HelloWorld from './components/hello-world';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +9,11 @@ class App extends Component {
   }
 
   render() {
-    return <HelloWorld title="Hello from React webpack" />;
+    return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    );
   }
 }
 
